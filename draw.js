65365fd6cf50734537043d7f0cd784619dcb7ffa
Mateusz Psujek <mateusz.psujek@gmail.com>
2018-06-20 22:53:17 +0200

    Fixed problems with refreshing, need to add detection collision with body

--- draw.js
+++ draw.js
@@ -10,7 +10,7 @@ var drawElements = function() {
     ctx.fillRect(x * snakeSize, y * snakeSize, snakeSize, snakeSize);
   }
 
-  var score = function() {
+  var showScore = function() {
     var scoreText = 'Score: ' + score;
     ctx.fillStyle = 'black';
     ctx.fillText(scoreText, 145, h - 5);
@@ -25,34 +25,10 @@ var drawElements = function() {
     }
   }
 
-  var createFood = function() {
-    food = {
-      x: Math.floor((Math.random() * 30) + 1),
-      y: Math.floor((Math.random() * 30) + 1)
-    }
-
-    for (var i = 0; i > snake.length; i++) {
-      var snakeX = snake[i].x;
-      var snakeY = snake[i].y;
-
-      if (food.x === snakeX || food.y === snakeY || food.y === snakeY && food.x === snakeX) {
-        food.x = Math.floor((Math.random() * 30) + 1);
-        food.y = Math.floor((Math.random() * 30) + 1);
-      }
-    }
-  }
-
-  var checkCollision = function(x, y, array) {
-    for (var i = 0; i < array.length; i++) {
-      if (array[i].x === x && array[i].y === y) {
-        return true;
-      }
-      return false;
-    }
-  }
-
   var paint = function() {
-    // btn.setArttribute('disabled', true);
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, w, h);
+
     var snakeX = snake[0].x;
     var snakeY = snake[0].y;
 
@@ -67,10 +43,10 @@ var drawElements = function() {
     }
 
     if (snakeX == -1 || snakeX == w / snakeSize || snakeY == -1 || snakeY == h / snakeSize || checkCollision(snakeX, snakeY, snake)) {
-      btn.removeAttribute('disabled', true);
 
       ctx.clearRect(0, 0, w, h);
       gameloop = clearInterval(gameloop);
+
       return;
     }
 
@@ -94,21 +70,46 @@ var drawElements = function() {
     }
 
     apple(food.x, food.y);
-    scoreText();
+    showScore();
+    console.log(score);
+  }
+
+  var createFood = function() {
+    food = {
+      x: Math.floor((Math.random() * 30) + 1),
+      y: Math.floor((Math.random() * 30) + 1)
+    }
+
+    for (var i = 0; i > snake.length; i++) {
+      var snakeX = snake[i].x;
+      var snakeY = snake[i].y;
+
+      if (food.x === snakeX || food.y === snakeY || food.y === snakeY && food.x === snakeX) {
+        food.x = Math.floor((Math.random() * 30) + 1);
+        food.y = Math.floor((Math.random() * 30) + 1);
+      }
+    }
+  }
+
+  var checkCollision = function(x, y, array) {
+    for (var i = 0; i < array.length; i++) {
+      if (array[i].x === x && array[i].y === y) {
+        return true;
+        score = 0;
+      }
+      return false;
+    }
+  }
+
+  var init = function() {
+    score = 0;
+    direction = 'down';
+    drawSnake();
+    createFood();
+    gameloop = setInterval(paint, 80);
 
   }
 
-  var init = function(){
-      direction = 'down';
-      drawSnake();
-      createFood();
-      gameloop = setInterval(paint, 80);
-  }
-
-
-    return {
-      init : init
-    };
-
+  return {init: init};
 
 }();
